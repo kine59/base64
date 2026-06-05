c5c55078c67730e2535dcb52cac2f1b479f97ad3
kine59 <159332722+kine59@users.noreply.github.com>
2026-06-05 02:57:01 -0400

    Refactor Base64 encoding/decoding logic with switch

--- Base64.js
+++ Base64.js
@@ -5,20 +5,31 @@ For Windows 93, albeit it can be used in any other website
 */
 let choice = prompt("1 - Decode\n2 - Encode\n3 - Exit") // choices for base64
 
-if (choice == "1") // decoding
+switch (choice)
 {
-  let base64decode = prompt("Put base64 text here");
-  if (base64decode)
-  {
-    // window added so it could show up in windows93, albeit you could remove this if you're using this for something else
-    window.alert(atob(base64decode)); // atob and btoa decode and encode base64 respectivefully
-  }
-}
-else if (choice == "2") // encode
-{
-  let base64encode = prompt("Put text here")
-  if (base64encode)
-  {
-    window.alert(btoa(base64encode));
-  }
+  case "1": // decode
+    let base64decode = prompt("Put base64 text here");
+
+    if (base64decode)
+    {
+      var decoded = atob(base64decode);
+      window.alert(decoded); // atob and btoa decode and encode base64 respectivefully
+      console.log(decoded)
+    }
+    break;
+  case "2": // encode
+    let base64encode = prompt("Put text here")
+    if (base64encode)
+    {
+      var encoded = btoa(base64encode);
+      window.alert(encoded);
+      console.log(encoded);
+    }
+    break;
+  case "3": // closing
+    window.alert("Script aborted");
+    break;
+  default: // invalid choice
+    window.alert("Invalid");
+    break;
 }
